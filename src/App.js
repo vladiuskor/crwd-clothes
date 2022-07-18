79e4ce5c3d5bfc3a18e52ef6a754a0e024cbc7b2
vladko <max.vlad.kor@gmail.com>
2022-07-18 17:00:51 +0300

    Added navigation to Shop page and back to main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import {Routes, Route} from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
+const Shop = () => {
+    return (
+        <h2>I am a Shop page</h2>
+    )
+}
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />}/>
+                <Route path="shop" element={<Shop />}/>
+            </Route>
         </Routes>
     );
 }
