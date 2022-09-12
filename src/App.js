8ee17534a1028830c6f1e07c3820f4d0fe24eb83
vladko <max.vlad.kor@gmail.com>
2022-09-12 19:11:25 +0300

    Added my old code without credentials

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Routes, Route} from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+    return (
+        <h2>I am a Shop page</h2>
+    )
+}
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />}/>
+                <Route path="shop" element={<Shop />}/>
+                <Route path="sign-in" element={<SignIn />}/>
+            </Route>
+        </Routes>
+    );
+}
+
+export default App;
