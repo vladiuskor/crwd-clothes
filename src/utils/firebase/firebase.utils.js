f8cff0cf3cb447b1af12b6bf191d5d563a011f88
Vlad Korobko <max.vlad.kor@gmail.com>
2022-09-22 17:28:27 +0300

    Made my code looking like teacher's code

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -78,6 +78,5 @@ export const signOutUser = async () => {
     await signOut(auth);
 };
 
-export const onAuthStateChangedListener = (callback) => {
+export const onAuthStateChangedListener = (callback) =>
     onAuthStateChanged(auth, callback);
-};
