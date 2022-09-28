2107653ea943cfbfa1d08184b7c8bea9fd6cc3c3
Vlad Korobko <max.vlad.kor@gmail.com>
2022-09-28 23:54:08 +0300

    Added functionality which delete cart item after clicking on cross

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,14 @@
 import './checkout-item.styles.scss';
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
 
 const CheckoutItem = ({cartItem}) => {
     const {name, imageUrl, price, quantity} = cartItem;
+
+    const {clearItemFromCart, addItemToCart, removeItemFromCart} = useContext(CartContext);
+    const clearItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemFromCart(cartItem);
 
     return (
         <div className="checkout-item-container">
@@ -10,10 +17,15 @@ const CheckoutItem = ({cartItem}) => {
             </div>
 
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+
+            <span className='quantity'>
+                <div className="arrow" onClick={removeItemHandler}>&#10094;</div>
+                <span className="value">{quantity}</span>
+                <div className="arrow" onClick={addItemHandler}>&#10095;</div>
+            </span>
             <span className='price'>{price}</span>
 
-            <div className="remove-button">&#10005;</div>
+            <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
         </div>
     )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -32,12 +32,17 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     }
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter( item => item.id !== cartItemToClear.id)
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0
 });
 
@@ -61,7 +66,11 @@ export const CartProvider = ({children}) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, cartItems, cartCount};
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear))
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartCount};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
