8cd4fb822498a956a26eb688dac45dc3eac4c371
Vlad Korobko <max.vlad.kor@gmail.com>
2022-10-04 15:55:00 +0300

    Added category page with broken styles

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,29 @@
+import {useContext, useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+
+import {CategotiesContext} from "../../contexts/categoties.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import './category.styles.scss';
+
+const Category = () => {
+    const {category} = useParams();
+    const {categoriesMap} = useContext(CategotiesContext);
+
+    const [products, setProducts] = useState(categoriesMap[category]);
+
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap]);
+
+    return (
+        <div className="category-container">
+            {
+                products && products.map((product) => <ProductCard key={product.id} product={product}/>)
+            }
+        </div>
+    )
+}
+
+export default Category;
