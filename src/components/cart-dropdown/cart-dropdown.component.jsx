01c085d7440de340827e842085f71b927eec87ae
Vlad Korobko <max.vlad.kor@gmail.com>
2022-10-18 15:46:35 +0300

    Rewrote cart-dropdown to styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from "./cart-dropdown.styles";
 
 import {useContext} from "react";
 import {useNavigate} from "react-router-dom";
@@ -17,12 +17,17 @@ const CartDropdown = () => {
     };
 
     return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? (
+                    cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)
+                ) : (
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
+                )}
+
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
