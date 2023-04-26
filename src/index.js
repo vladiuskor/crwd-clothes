cf85d40891458f4f5d3ddc27477fc3a09ddccd54
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-26 16:31:21 +0300

    Passed section 5 (React-Router)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
 
@@ -9,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
